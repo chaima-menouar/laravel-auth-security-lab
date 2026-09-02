8413d7402f3534e3068fd99ad2f6d621146d0b2c
Chaima Menouar <chaimaamnouar1234@gmail.com>
2026-09-03 00:47:12 +0100

    Refactor middleware and exception handling signatures

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,9 +11,16 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->redirectGuestsTo(
+            fn (Request $request) => route('login.standard.form')
+        );
+
+        $middleware->redirectUsersTo(
+            fn (Request $request) => route('dashboard')
+        );
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+    ->create();
